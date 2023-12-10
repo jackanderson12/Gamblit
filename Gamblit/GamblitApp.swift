f24b1a13edd0d9ebd788e7e8c15eda9357f70e25
jackanderson12 <43770968+jackanderson12@users.noreply.github.com>
2023-12-10 10:43:18 -0500

    Initial Commit

---
+++ Gamblit/GamblitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamblitApp.swift
+//  Gamblit
+//
+//  Created by Jack Anderson on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GamblitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
